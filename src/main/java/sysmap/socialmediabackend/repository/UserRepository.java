2dc2875247824f75096b185574f7c148e3ef99d5
Jiripoz <103076776+Jiripoz@users.noreply.github.com>
2023-05-04 07:42:27 -0300

    added new TestController to test user privileges/roles | minor refactor to application.properties and our main Application

--- src/main/java/sysmap/socialmediabackend/repository/UserRepository.java
+++ src/main/java/sysmap/socialmediabackend/repository/UserRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
-  Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-  Boolean existsByUsername(String username);
+    Optional<User> findByEmail(String email);
 
-  Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
