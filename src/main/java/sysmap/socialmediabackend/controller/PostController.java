d45464c98a31c6b32e9c9061e54e6ea4e5d8ed61
Jiripoz <103076776+Jiripoz@users.noreply.github.com>
2023-05-08 00:57:22 -0300

    added likes and comments to posts and made changes to PostController to handle these requests

--- src/main/java/sysmap/socialmediabackend/controller/PostController.java
+++ src/main/java/sysmap/socialmediabackend/controller/PostController.java
@@ -1,13 +1,17 @@
 package sysmap.socialmediabackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 import sysmap.socialmediabackend.model.Post;
 import sysmap.socialmediabackend.model.User;
+import sysmap.socialmediabackend.model.postfeatures.Comment;
+import sysmap.socialmediabackend.model.postfeatures.Like;
 import sysmap.socialmediabackend.repository.PostRepository;
 import sysmap.socialmediabackend.repository.UserRepository;
 
@@ -33,13 +37,9 @@ public class PostController {
     @PostMapping
     public Post createPost(@RequestBody Post post, HttpServletRequest request, Authentication authentication) throws Exception {
         post.setCreatedAt(LocalDateTime.now());
-        System.out.println(userRepository.findAll());
-        String currentUserName = authentication.getName();
-        System.out.println("The current username is: "+currentUserName);
-        User currentUser = userRepository.findByUsername(currentUserName)
-                                         .orElseThrow(() -> new Exception("User not found"));
-        post.setUser(currentUser);
-        post.setAuthor(currentUserName);
+        User user = getCurrentUser();
+        post.setUser(user);
+        post.setAuthor(user.getUsername());
         return postRepository.save(post);
     }
 
@@ -60,5 +60,43 @@ public class PostController {
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable String id) {
         postRepository.deleteById(id);
+    }
+
+    // Add a comment to a post
+    @PostMapping("/{id}/comments")
+    public ResponseEntity<Post> addComment(@PathVariable String id, @RequestBody Comment comment) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            comment.setUser(getCurrentUser());
+            post.addComment(comment);
+            postRepository.save(post);
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/{id}/likes")
+    public ResponseEntity<Post> addLike(@PathVariable String id, @RequestBody Like like) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            User currentUser = getCurrentUser();
+            if (!post.getLikes().contains(like)) {
+                like.setUser(currentUser);
+                post.addLike(like);
+                postRepository.save(post);
+            }
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        return userRepository.findByUsername(currentUserName).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + currentUserName));
     }
 }
